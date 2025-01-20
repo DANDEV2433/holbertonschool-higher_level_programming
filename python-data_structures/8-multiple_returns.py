0db47125485399c1258a52e18bd597da005fa425
DANDEV2433 <alexdani3344@gmail.com>
2025-01-20 19:46:16 +0100

    len and first caracter

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    first_char = sentence[0]
+    if length > 0:
+        first_char = sentence[0]
+    else:
+        None
     return length, first_char
