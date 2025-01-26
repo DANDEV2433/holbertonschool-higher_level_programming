e676ee11d595c9f1e1af13a7c64d6b67bcb67a8a
DANDEV2433 <alexdani3344@gmail.com>
2025-01-26 10:51:38 +0100

    print <first_name><last_name>

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -19,12 +19,11 @@ Parameters:
     Raises:
     TypeError: first_name must be a string or last_name must be a string
     """
-    if not isinstance(first_name, str)or first_name.isdigit():
+    if not isinstance(first_name, str) or first_name.isdigit():
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    
     if last_name == "":
-        print("My name is {}".format(first_name))
+        print("My name is {} ".format(first_name))
     else:
         print("My name is {} {}".format(first_name, last_name))
