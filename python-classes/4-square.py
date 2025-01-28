790d1e9487bb6f4b61675e8e227e369faf71bbf2
DANDEV2433 <alexdani3344@gmail.com>
2025-01-28 11:22:24 +0100

    use getter and setter

---
+++ python-classes/4-square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class with a size with test Error
+"""
+
+
+class Square:
+    """
+    A class that defines a square.
+    """
+    def __init__(self, size=0):
+        """
+        Initializes the square with a size 0
+
+        Args:
+            size (int): The size of the square.
+            raise:  ValueError : if size is < O
+                    TypeError  : if size isn't an integer
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size  # Utilise self pour définir l'attribut size
+
+    def area(self):
+        """
+        returns the current square area
+        """
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """
+        function getter for retrieve size
+
+        Args:
+            None
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        function setter for set size
+
+        Args:  Value of the size
+
+        raise:  ValueError : if size is < O
+                TypeError  : if size isn't an integer
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
