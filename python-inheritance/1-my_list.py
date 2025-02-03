cf18eff4a23d6be98740c5b9b0742dcd00b8017b
DANDEV2433 <alexdani3344@gmail.com>
2025-02-03 16:11:23 +0100

    prints the list, but sorted

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+class MyList that inherits from list
+"""
+
+
+class MyList(list):
+    """
+    class MyList that inherits from list
+    """
+    pass
+
+    def print_sorted(self):
+        """
+        Public instance method that prints the list, but sorted
+        """
+        print(sorted(self))
