7a4dfaa862aa97c1990df6f80e4f1f7e8ada8ffa
DANDEV2433 <alexdani3344@gmail.com>
2025-02-04 15:57:46 +0100

    return area of Rectangle

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,37 +1,9 @@
 #!/usr/bin/python3
 """
-initialize class Rectangle with your width and height with
-integer_validator method and pass to private
+Write a class Square that inherits from Rectangle
 """
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
-
-
-class Rectangle(BaseGeometry):
-    """
-    class Rectangle that inherits from BaseGeometry (7-base_geometry.py).
-    """
-    def __init__(self, width, height):
-        """
-        Instantiation with width and height
-        check attribut with integer_validator and pass to private
-        """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """
-        Calculate and return the area of the rectangle
-        """
-        return self.__width * self.__height
-
-    def __str__(self):
-        """
-        Return rectangle description
-        """
-        return f"[Rectangle] {self.__width}/{self.__height}"
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
@@ -51,12 +23,6 @@ class Square(Rectangle):
         """
         super().__init__(size, size)
         self.__size = size
-
-    def __str__(self):
-        """
-        return rectangle description
-        """
-        return f"[Rectangle] {self.__size}/{self.__size}"
 
     def area(self):
         """
