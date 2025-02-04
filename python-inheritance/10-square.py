65366db11eb75ad5ee1e195a6a108714b6f8a3bf
DANDEV2433 <alexdani3344@gmail.com>
2025-02-04 15:51:52 +0100

    return area of Rectangle

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -21,6 +21,18 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.__height = height
 
+    def area(self):
+        """
+        Calculate and return the area of the rectangle
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        Return rectangle description
+        """
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
 
 class Square(Rectangle):
     """
