62a94eef29f1b1be6f15bbd4f13133fadbcd6213
DANDEV2433 <alexdani3344@gmail.com>
2025-02-05 14:10:19 +0100

    Setting up the Abstract Class Animal

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,21 +1,34 @@
 #!/usr/bin/python3
 """
-Setting up the Abstract Class Animal
+import ABC and abstractmethod module abc
 """
 from abc import ABC, abstractmethod
 
+"""
+Setting up the Abstract Class Animal
+"""
+
 
 class Animal(ABC):
+    """
+    the Animal class, ensuring it inherits from ABC to mark it as abstract.
+    """
     @abstractmethod
     def sound(self):
         pass
 
 
 class Dog(Animal):
+    """
+    Create a subclass named Dog that inherits from the Animal class.
+    """
     def sound(self):
         return "Bark"
 
 
 class Cat(Animal):
+    """
+    Create a subclass named Cat that inherits from the Animal class.
+    """
     def sound(self):
         return "Meow"
