91ec516c4ff3e4c5166a085f96919d9af27c14e3
DANDEV2433 <alexdani3344@gmail.com>
2025-02-05 11:50:58 +0100

    Setting up the Abstract Class Animal

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Setting up the Abstract Class Animal
+"""
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    @abstractmethod
+    def sound(self):
+        pass
+
+
+class Dog(Animal):
+    def sound(self):
+        return "Bark"
+
+
+class Cat(Animal):
+    def sound(self):
+        return "Meow"
