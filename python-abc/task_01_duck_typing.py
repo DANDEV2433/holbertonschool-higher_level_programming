bfc036d333b22e38e0b934bddeb501bf55d8b42e
DANDEV2433 <alexdani3344@gmail.com>
2025-02-05 15:37:45 +0100

    area and perimeter of Circle and Rectangle

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+import math
+"""
+import ABC and abstractmethod module abc
+"""
+from abc import ABC, abstractmethod
+
+"""
+Setting up the Abstract Class Shape
+"""
+
+
+class Shape(ABC):
+    """
+    the Shape class, ensuring it inherits from ABC to mark it as abstract.
+    """
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+
+class Circle(Shape):
+    """
+    concrete class Circle inheriting from Shape
+    """
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius ** 2
+
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+
+
+class Rectangle(Shape):
+    """
+    concrete class Rectangle inheriting from Shape
+    """
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+
+def shape_info(Shape):
+    print("area: {}".format(Shape.area()))
+    print("perimeter: {}".format(Shape.perimeter()))
