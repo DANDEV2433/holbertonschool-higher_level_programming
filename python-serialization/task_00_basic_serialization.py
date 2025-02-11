1da4efb0d7982c73ca30aeb5e450dc0243140454
DANDEV2433 <alexdani3344@gmail.com>
2025-02-11 23:17:41 +0100

    serialized and deserialized

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+basic serialization module that adds the functionality
+to serialize a Python dictionary to a JSON file
+and deserialize the JSON file to recreate the Python Dictionary.
+"""
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    with open(filename, "w", encoding="utf_8") as file:
+        json.dump(data, file)
+
+
+def load_and_deserialize(filename):
+    with open(filename, "r", encoding="utf-8") as file:
+        return json.load(file)
