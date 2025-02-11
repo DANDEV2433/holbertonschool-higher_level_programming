8981dca5fd28e866d8ee2ccce7342a8f702f745c
DANDEV2433 <alexdani3344@gmail.com>
2025-02-11 23:45:05 +0100

    start task 1

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+basic serialization module that adds the functionality
+to serialize a Python dictionary to a JSON file
+and deserialize the JSON file to recreate the Python Dictionary.
+"""
+import json
+import pickle
+
+class CustomObject(name, age, is_student):
