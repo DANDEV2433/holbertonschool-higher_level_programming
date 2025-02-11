fdba0ed188a0ddc488d4ca0a1ba80b0d2ea93563
DANDEV2433 <alexdani3344@gmail.com>
2025-02-11 11:29:49 +0100

    Module JSON, Write a function that writes an Object to a text file

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -10,4 +10,4 @@ def save_to_json_file(my_obj, filename):
     Write a function that writes an Object to a text file
     """
     with open(filename, "w", encoding="utf_8") as file:
-        json.dumps(my_obj)
+        json.dump(my_obj, file)
