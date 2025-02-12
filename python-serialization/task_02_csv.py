a1ba4b0589488bea215755922848ee7c5c99dd8a
DANDEV2433 <alexdani3344@gmail.com>
2025-02-12 16:56:56 +0100

    csv and json

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+module csv and json
+"""
+import csv
+import json
+
+
+def convert_csv_to_json(csv_filename):
+    try:
+        with open(csv_filename, 'r', encoding='utf-8') as csvfile:
+            """
+            csv reader read csv file and convert line in dictionary
+            """
+            csv_reader = csv.DictReader(csvfile)
+            for row in csv_reader:
+                print(row)
+
+        with open('data.json', "w", encoding='utf-8') as jsonfile:
+            """
+            Writes the data list to the JSON file using 4-space indentation
+            """
+            json.dump('data.json', jsonfile, indent=4)
+            return True
+    except FileNotFoundError:
+        return False
