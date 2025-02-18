671a6cc0c660feba387089e547e3ec663761f775
DANDEV2433 <alexdani3344@gmail.com>
2025-02-18 21:58:40 +0100

    task 2

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -19,22 +19,19 @@ def fetch_and_save_posts():
     r = requests.get("https://jsonplaceholder.typicode.com/posts")
     print(f"Status Code: {r.status_code}")
     if r.status_code == 200:
-        """
-    Analyse les données JSON de la réponse
-    et les stocke dans la variable posts.
-        """
-    posts = r.json()
-    new_posts = [
-        {
-            "id": post["id"],
-            "title": post["title"],
-            "body": post["body"]
-        }for post in posts
-                ]
+        # Analyse données JSON de la réponse et stocke dans la variable posts.
+        posts = r.json()
+        new_posts = [
+            {
+                "id": post["id"],
+                "title": post["title"],
+                "body": post["body"]
+            }for post in posts
+        ]
     with open('post.csv', 'w', newline='', encoding='utf-8') as csvfile:
-        column_names = ["id", "title", "body"]
+        fieldnames = ["id", "title", "body"]
         # Crée un objet DictWriter qui utilise les noms des colonnes définis.
-        writer = csv.DictWriter(csvfile, column_names)
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         writer.writerows(new_posts)
 
