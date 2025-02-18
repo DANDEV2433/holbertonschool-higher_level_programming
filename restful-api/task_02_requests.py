9208a0635e6e6e84ce807cdcdcb0750371130edb
DANDEV2433 <alexdani3344@gmail.com>
2025-02-18 21:38:25 +0100

    task_02

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Consuming and processing data from an API using Python
+"""
+import csv
+import requests
+
+
+def fetch_and_print_posts():
+    r = requests.get("https://jsonplaceholder.typicode.com/posts")
+    print(f"Status Code: {r.status_code}")
+    if r.status_code == 200:
+        posts = r.json()
+        for post in posts:
+            print(post["title"])
+
+
+def fetch_and_save_posts():
+    r = requests.get("https://jsonplaceholder.typicode.com/posts")
+    print(f"Status Code: {r.status_code}")
+    if r.status_code == 200:
+        """
+    Analyse les données JSON de la réponse
+    et les stocke dans la variable posts.
+        """
+    posts = r.json()
+    new_posts = [
+        {
+            "id": post["id"],
+            "title": post["title"],
+            "body": post["body"]
+        }for post in posts
+                ]
+    with open('post.csv', 'w', newline='', encoding='utf-8') as csvfile:
+        column_names = ["id", "title", "body"]
+        # Crée un objet DictWriter qui utilise les noms des colonnes définis.
+        writer = csv.DictWriter(csvfile, column_names)
+        writer.writeheader()
+        writer.writerows(new_posts)
+
+
+fetch_and_print_posts()
+fetch_and_save_posts()
