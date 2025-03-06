d4f3a552928dc0c0f48dd838b5e090b11cd29e0a
DANDEV2433 <alexdani3344@gmail.com>
2025-03-06 21:22:36 +0100

    LIKE 'N%'

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+script that lists all states with a name starting with N (upper N)
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    # Connexion à la base de données MySQL
+    db = MySQLdb.connect(
+        host='localhost',
+        user=mysql_username,
+        password=mysql_password,
+        database=database_name,
+        port=3306
+    )
+
+    # Création d'un objet curseur
+    cursor = db.cursor()
+    # Exécuter la requête SQL
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    # Récupérer tous les résultats de la requête
+    states = cursor.fetchall()
+
+    # Afficher chaque état
+    for state in states:
+        print(state)
+    # Fermer le curseur et la connexion
+    cursor.close()
+    db.close()
