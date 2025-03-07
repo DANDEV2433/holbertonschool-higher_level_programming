6d06d29601cc25a9799f23548cb69ef36d2b0dd7
DANDEV2433 <alexdani3344@gmail.com>
2025-03-07 22:41:43 +0100

    task7

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+script that lists all the State objects in the hbtn_0e_6_usa database
+"""
+
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+# créer un moteur de base de données
+from sqlalchemy import create_engine
+import sys
+
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+    mysql_username,
+    mysql_password,
+    database_name
+    ),
+    pool_pre_ping=True)
+# Création de toutes les tables
+# définies par les sous-classes de Base (si elles n'existent pas déjà)
+Base.metadata.create_all(engine)
+# Création d'une classe de session configurée
+Session = sessionmaker(bind=engine)
+# Création d'une instance de session
+session = Session()
+# Requête pour récupérer tous les objets State et les trier par id
+states = session.query(State).order_by(State.id).all()
+
+for State in states:
+    print('{}, {}'.format(State.id, State.name))
+          
+session.close()
