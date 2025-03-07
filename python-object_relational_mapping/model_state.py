7699f71904a5616465768703ac8be13d4827c689
DANDEV2433 <alexdani3344@gmail.com>
2025-03-07 18:30:36 +0100

    Write a python file that contains the class definition of a State and an instance Base = declarative_base()

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Defines a State class and Base instance for ORM
+"""
+
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+class State(Base):
+    """
+    State class that links to the MySQL table states
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
