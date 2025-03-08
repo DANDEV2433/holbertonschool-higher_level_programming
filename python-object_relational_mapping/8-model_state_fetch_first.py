13027f5bef2a5689fdb3ae95c0c143add83a7484
DANDEV2433 <alexdani3344@gmail.com>
2025-03-08 10:26:21 +0100

    task 8

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -12,20 +12,19 @@ if __name__ == "__main__":
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-    sys.argv[1],
-    sys.argv[2],
-    sys.argv[3]
-    ),
-    pool_pre_ping=True)
-# Création d'une classe de session configurée
-Session = sessionmaker(bind=engine)
-# Création d'une instance de session
-session = Session()
-# Requête pour récupérer tous les objets State et les trier par id
-State = session.query(State).order_by(State.id).first()
-if State:
-    print('{}: {}'.format(State.id, State.name))
-else:
-    print("Nothing")
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        mysql_username,
+        mysql_password,
+        database_name
+    ), pool_pre_ping=True)
+    # Création d'une classe de session configurée
+    Session = sessionmaker(bind=engine)
+    # Création d'une instance de session
+    session = Session()
+    # Requête pour récupérer tous les objets State et les trier par id
+    State = session.query(State).order_by(State.id).first()
+    if State:
+        print('{}: {}'.format(State.id, State.name))
+    else:
+        print("Nothing")
 session.close()
